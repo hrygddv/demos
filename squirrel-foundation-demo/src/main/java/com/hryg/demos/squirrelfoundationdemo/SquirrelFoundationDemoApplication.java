dc0c623d8f09ce814db17d79f129314eba9623d0
hrygddv <hengrui@topode.com>
2021-09-18 17:58:18 +0800

    增加 squirrel demo

---
+++ squirrel-foundation-demo/src/main/java/com/hryg/demos/squirrelfoundationdemo/SquirrelFoundationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hryg.demos.squirrelfoundationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirrelFoundationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquirrelFoundationDemoApplication.class, args);
+    }
+
+}
---
+++ squirrel-foundation-demo/src/test/java/com/hryg/demos/squirrelfoundationdemo/SquirrelFoundationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hryg.demos.squirrelfoundationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquirrelFoundationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
